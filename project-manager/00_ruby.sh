efb3964a7ef732002170267f027db20de4c866cc
Alex <litvinok@gmail.com>
2013-06-19 13:10:41 +0300

    Update and rename 00_prepare.sh to 00_ruby.sh

---
+++ project-manager/00_ruby.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+curl -O http://www6.atomicorp.com/channels/atomic/centos/6/x86_64/RPMS/atomic-release-1.0-14.el6.art.noarch.rpm
+rpm -Uvh atomic-release-1.0-14.el6.art.noarch.rpm
+rpm -Uvh http://rbel.co/rbel5
+
+yum update -y
+yum groupinstall "Development Tools" -y
+yum install libyaml-devel mysql mysql-devel mysql-lib ImageMagick-devel ImageMagick-c++-devel -y
+
+curl -L https://get.rvm.io | bash
+source /etc/profile
+
+type rvm | head -n 1
+
+rvm pkg install libyaml
+rvm requirements
+rvm install 1.9.3
+rvm use 1.9.3 --default
+rvm rubygems current
+
+gem install bundler
+gem install rake
+gem install rails
+gem install mysql2
+gem install rmagick
