4451524c73fe84b960a98b110e298783132a3164
Alex <litvinok@gmail.com>
2013-06-19 17:45:30 +0300

    Update 20_git.sh

--- 20_git.sh
+++ 20_git.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-yum install sudo vim -y
+yum install sudo vim libicu-devel -y
+gem install charlock_holmes --version '0.6.9.4'
 
 useradd -U -d /home/git -c "GitLab" git
 echo "git  ALL=(ALL)  ALL" >> /etc/sudoers
@@ -23,3 +24,39 @@ mysql -e "FLUSH PRIVILEGES;"
 cd /home/git
 
 sudo -u git -H git clone https://github.com/gitlabhq/gitlabhq.git gitlab
+cd /home/git/gitlab
+sudo -u git -H git checkout 5-2-stable
+sudo -u git -H cp config/gitlab.yml.example config/gitlab.yml
+
+chown -R git log/
+chown -R git tmp/
+chmod -R u+rwX  log/
+chmod -R u+rwX  tmp/
+
+sudo -u git -H mkdir /home/git/gitlab-satellites
+
+sudo -u git -H mkdir tmp/pids/
+sudo -u git -H mkdir tmp/sockets/
+chmod -R u+rwX  tmp/pids/
+chmod -R u+rwX  tmp/sockets/
+
+sudo -u git -H mkdir public/uploads
+chmod -R u+rwX  public/uploads
+
+sudo -u git -H cp config/puma.rb.example config/puma.rb
+sudo -u git -H git config --global user.name "GitLab"
+sudo -u git -H git config --global user.email "gitlab@localhost"
+
+cat << 'EOF' > config/database.yml
+production:
+  adapter: mysql2
+  encoding: utf8
+  reconnect: false
+  database: gitlabhq
+  pool: 5
+  username: gitlab
+  password: eeL7owed
+EOF
+
+cd /home/git/gitlab
+bundle install --deployment --without development test postgres
