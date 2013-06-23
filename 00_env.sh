8580bcf1f9524090084a0905349dd555224f4b4e
Alex <litvinok@gmail.com>
2013-06-23 17:50:50 +0300

    Update 00_env.sh

--- 00_env.sh
+++ 00_env.sh
@@ -6,7 +6,7 @@ rpm -Uvh http://rbel.co/rbel5
 
 yum update -y
 yum groupinstall "Development Tools" -y
-yum install libyaml-devel mysql mysql-devel mysql-lib ImageMagick-devel ImageMagick-c++-devel libtool nginx apr-devel apr curl-devel-y
+yum install libyaml-devel mysql mysql-devel mysql-lib ImageMagick-devel ImageMagick-c++-devel libtool nginx apr-devel apr curl-devel -y
 
 curl -L https://get.rvm.io | bash
 source /etc/profile
