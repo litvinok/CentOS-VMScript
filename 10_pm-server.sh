6e3df3a7722a9fe760da84234f0e75dc498aa499
Alex <litvinok@gmail.com>
2013-06-24 18:52:14 +0300

    Update and rename 10_redmine.sh to 10_pm-server.sh

--- 10_pm-server.sh
+++ 10_pm-server.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-yum install wget mysql-server -y
-service mysqld start
+yum install nginx redis -y
+
+gem install rake
+gem install rails
+gem install mysql2
+gem install rmagick
+gem install vpim
+
+gem install charlock_holmes --version '0.6.9.4'
+
+# ---------------------------------------------------------------------------------------------
 
 cd /home
 git clone https://github.com/edavis10/redmine.git
@@ -74,3 +83,64 @@ rm -f redminecrm-0_0_1.zip
 git clone https://github.com/pixel-cookers/redmine-theme.git pixel-cookers
 
 cd /home/redmine
+
+# ------------------------------------------------------------------------
+
+useradd -U -d /home/git -c "GitLab" git
+echo "git  ALL=(ALL)  ALL" >> /etc/sudoers
+
+sudo su git
+cd /home/git
+git clone https://github.com/gitlabhq/gitlab-shell.git
+cd gitlab-shell
+git checkout v1.4.0
+cp config.yml.example config.yml
+./bin/install
+
+
+mysql -e "CREATE USER 'gitlab'@'localhost' IDENTIFIED BY 'eeL7owed';"
+mysql -e "CREATE DATABASE IF NOT EXISTS gitlabhq DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql -e "GRANT SELECT, LOCK TABLES, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON gitlabhq.* TO gitlab@localhost;"
+mysql -e "FLUSH PRIVILEGES;"
+
+
+cd /home/git
+
+git clone https://github.com/gitlabhq/gitlabhq.git gitlab
+cd /home/git/gitlab
+git checkout 5-2-stable
+cp config/gitlab.yml.example config/gitlab.yml
+
+chown -R git log/
+chown -R git tmp/
+chmod -R u+rwX  log/
+chmod -R u+rwX  tmp/
+
+mkdir /home/git/gitlab-satellites
+
+mkdir tmp/pids/
+mkdir tmp/sockets/
+chmod -R u+rwX  tmp/pids/
+chmod -R u+rwX  tmp/sockets/
+
+mkdir public/uploads
+chmod -R u+rwX  public/uploads
+
+cp config/puma.rb.example config/puma.rb
+git config --global user.name "GitLab"
+git config --global user.email "gitlab@localhost"
+
+cat << 'EOF' > config/database.yml
+production:
+  adapter: mysql2
+  encoding: utf8
+  reconnect: false
+  database: gitlabhq
+  pool: 5
+  username: gitlab
+  password: eeL7owed
+EOF
+
+cd /home/git/gitlab
+bundle install --deployment --without development test postgres
+bundle exec rake gitlab:setup RAILS_ENV=production
